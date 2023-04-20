ce6db77816558f0f59b04277407ed4056c5d98c4
Winston Heard <winston.nolen@gmail.com>
2023-04-20 03:22:54 -0700

    update with apollo dependencies

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
-import { AuthenticationError } from 'apollo-server-express';
-import { User } from '../models';
-import { signToken } from '../utils/auth';
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
